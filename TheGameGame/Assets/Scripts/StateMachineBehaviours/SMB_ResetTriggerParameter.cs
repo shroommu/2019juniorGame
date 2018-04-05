e5d64fff100602acf8db22bf27506b8409594c7c
Unknown <genaralskar1@gmail.com>
2018-04-05 14:05:02 -0600

    add jim animations in collin scene

---
+++ TheGameGame/Assets/Scripts/StateMachineBehaviours/SMB_ResetTriggerParameter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMB_ResetTriggerParameter : StateMachineBehaviour {
+
+	//Made by: Collin Anderson
+
+	[Tooltip("This is the trigger param you want to reset")]
+	public string SetParam;
+
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		animator.ResetTrigger(SetParam);
+	}
+}
